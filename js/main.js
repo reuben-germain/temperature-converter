967ec3be9d91b489c303ff0bba8587cddc6742c2
Reuben Germain <reuben.germain@soprasteria.com>
2024-03-11 12:17:35 +0000

    created a convertThat function to see if I could get the value out of the input & put it into the console

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 document.querySelector("#convert").addEventListener('click', convertThat)
+
+function convertThat(){
+    const degree = document.querySelector('#temp').value
+    console.log(degree);
+}
